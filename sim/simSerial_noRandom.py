19bd02b514c0a8531926a6549596bc16b2d095ae
Harrison Foley <m211926@roger.navydsrc.hpc.mil>
2019-06-05 08:46:19 -0500

    removes random behavior for testing purposes

---
+++ sim/simSerial_noRandom.py
@@ -0,0 +1,186 @@
+import sys
+import time
+import numpy as np
+from array import *
+from matplotlib import pyplot as plt
+from math import ceil, floor, sqrt
+#import plotly.plotly as py
+#import plotly.tools as tls	
+
+
+if(len(sys.argv) < 3):
+	sys.exit("usage: python[3] simSerial_noRandom.py [num lattice points each side of square] [num timesteps]") 
+	time.sleep(1)
+
+N = int(sys.argv[1])
+t = int(sys.argv[2])
+
+
+class Lattice:
+	#static 2D array data structure for lattices
+	dirt = []
+	
+	#static counter (removes random functionality)
+	counter = 0
+
+	# states
+ 	# 0: dry
+	# 1: saturated on top, no water on top
+	# 2: not saturated, water on top
+	# 3: saturated and water on top (ultra saturated)
+	def __init__(self, x, y, state):
+		self.x = x
+		self.y = y
+		self.state = state
+		self.future = 0
+		self.neighbors = []
+		return
+
+	def __str__(self):
+		return str(self.state)
+
+	def calcNeighbors(self, size):
+		self.neighbors.append(Lattice.dirt[(self.x - 1) % size][(self.y - 1) % size])
+		self.neighbors.append(Lattice.dirt[(self.x - 1) % size][(self.y + 1) % size])
+		self.neighbors.append(Lattice.dirt[(self.x + 1) % size][(self.y - 1) % size])
+		self.neighbors.append(Lattice.dirt[(self.x + 1) % size][(self.y + 1) % size])
+		self.neighbors.append(Lattice.dirt[(self.x - 1) % size][self.y])
+		self.neighbors.append(Lattice.dirt[(self.x + 1) % size][self.y])
+		self.neighbors.append(Lattice.dirt[self.x][(self.y + 1) % size])
+		self.neighbors.append(Lattice.dirt[self.x][(self.y + 1) % size])
+
+	def computeIndex(self, size):
+		index = 0
+		for z in self.neighbors:
+			index += z.state
+		return index
+
+	def update(self):
+		self.state = self.future
+		return
+
+	def stageNextState(self, index, size):
+		index = self.computeIndex(size)
+		Lattice.counter += 1
+
+		#completely dry
+		#0.2% chance of getting rained on
+		#index 0-6: stays dry
+		#index 6-12: get saturated
+		#index 13-24: gets ultra saturated
+		if self.state == 0:
+			if Lattice.counter > 267:
+				self.future = 2
+				Lattice.counter = 0
+			elif index < 7:
+				self.future = 0
+			elif index < 17:
+				self.future = 1
+			else:
+				self.future = 3
+
+		#saturated with no water on top
+	   	#0.2% chance of ultra saturation (gets rained on)
+		#index 0-2: future dries up
+		#else: stays saturated 
+		elif self.state == 1:
+			if Lattice.counter > 267:
+				self.future = 3
+				Lattice.counter = 0
+			elif index > 17:
+				self.future = 3
+			elif index < 1:
+				self.future = 0
+			else:
+				self.future = 1
+
+		#water on top with no saturation
+		#50% chance of resting on top
+		#50% chance of saturating earth
+		elif self.state == 2:
+			if Lattice.counter % 2 == 1:
+				self.future = 2
+			else:
+				self.future = 1
+				Lattice.counter += 1
+			
+		#saturated with water on top	
+		#index 0-9 goes to just saturated state
+		#index 10-24 remains ultra-saturated
+		else:
+			if index < 8:
+				self.future = 1
+				Lattice.counter += 1
+			else:
+				self.future = 3
+
+
+class Game:
+	
+	def __init__(self, size, timeSteps):
+		self.size = size
+		self.timeSteps = timeSteps
+		self.lattices = Lattice.dirt
+		return
+
+	def setup(self):
+		print("Initializing...")
+		time.sleep(1)
+		for i in range(0, self.size):
+			temp = []
+			for j in range(0, self.size):	
+				l = Lattice(i,j,0)
+				temp.append(l)
+			self.lattices.append(temp)
+		#UNCOMMENT TO SEE LATTICE NEIGHBOR CALCULATION
+		#count = 0
+		for i in range(self.size):
+			for j in range(self.size):
+				self.lattices[i][j].calcNeighbors(self.size)
+				#UNCOMMENT TO SEE LATTICE CURRENTLY WORKING
+				#count += 1
+				#print("Lattice point: " + str(count))
+		return
+	
+
+	def stage(self):
+		for i in range(self.size):
+			for j in range(self.size):
+				index = self.lattices[i][j].computeIndex(self.size)
+				self.lattices[i][j].stageNextState(index, self.size)
+		return
+	
+	def update(self):
+		for i in range(self.size):
+			for j in range(self.size):
+				self.lattices[i][j].update()
+		return
+
+	def printPointAt(self, ex, why):
+		sys.stdout.write(str(self.lattices[ex][why]) + " ") 
+	
+	def printBoard(self):
+		for i in range(self.size):
+			for j in range(self.size):
+				self.printPointAt(i,j)
+			print("")
+
+	def play(self):
+		self.setup()
+		print("Simulation beginning...")
+		#time.sleep(1)		
+		for i in range(1, self.timeSteps):
+			self.stage()
+			self.update()
+			#UNCOMMENT IF YOU WANT TO SEE TIMESTEPS
+			#print("Timestep: " + str(i))
+		#print("Done! Printing board:")
+		#time.sleep(1)
+		self.printBoard()
+		print("*" * self.size + str(i) + "*" * (self.size-1))
+		#time.sleep(0.3)
+		return
+
+a = Game(N, t)
+a.play()
+
