184c3c4e21f324bcc35aa7e6ef34eabc88a4955e
Harrison Foley <m211926@roger.navydsrc.hpc.mil>
2019-06-06 15:45:31 -0500

    fixed bug impeding communication

--- sim/simParallel_procedural.py
+++ sim/simParallel_procedural.py
@@ -10,10 +10,10 @@ comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-random.seed(420)
+random.seed(rank)
 
-N = 512
-t = 100
+N = 50
+t = 900
 
 height = int(N/size)
 
@@ -61,8 +61,8 @@ def new_state(state, index):
 def update(timestep):
 	
 	#step 1: send appropriate rows
-	up_dest = (rank - 1) % size
-	down_dest = (rank + 1) % size
+	up_dest = int(rank - 1) % int(size)
+	down_dest = int(rank + 1) % int(size)
 
 	if timestep % 2 == 0:
 		comm.Isend(evenGrid[0, :], dest=up_dest)
@@ -71,6 +71,9 @@ def update(timestep):
 		comm.Isend(oddGrid[0, :], dest=up_dest)
 		comm.Isend(oddGrid[height - 1, :], dest=down_dest)
 
+	#receive top and bottom extension rows
+	req_up = comm.Irecv(top_halo, source=up_dest)
+	req_down = comm.Irecv(bot_halo, source=down_dest)
 
 	#for each lattice point NOT on edge, do an update
 	for i in range(1, height - 1):
@@ -79,23 +82,23 @@ def update(timestep):
 			#index calculation
 			index = 0
 			if timestep % 2 == 0:
-				index += evenGrid[(i - 1) % N, j] 
-				index += evenGrid[(i + 1) % N, j]
+				index += evenGrid[i - 1, j] 
+				index += evenGrid[i + 1, j]
 				index += evenGrid[i, (j + 1) % N] 
 				index += evenGrid[i, (j - 1) % N] 
-				index += evenGrid[(i - 1) % N, (j + 1) % N]
-				index += evenGrid[(i + 1) % N, (j - 1) % N] 
-				index += evenGrid[(i + 1) % N, (j + 1) % N] 
-				index += evenGrid[(i - 1) % N, (j - 1) % N] 
+				index += evenGrid[i - 1, (j + 1) % N]
+				index += evenGrid[i + 1, (j - 1) % N] 
+				index += evenGrid[i + 1, (j + 1) % N] 
+				index += evenGrid[i - 1, (j - 1) % N] 
 			else:
-				index += oddGrid[(i - 1) % N, j] 
-				index += oddGrid[(i + 1) % N, j]
+				index += oddGrid[i - 1, j] 
+				index += oddGrid[i + 1, j]
 				index += oddGrid[i, (j + 1) % N]
 				index += oddGrid[i, (j - 1) % N] 
-				index += oddGrid[(i - 1) % N, (j + 1) % N] 
-				index += oddGrid[(i + 1) % N, (j - 1) % N] 
-				index += oddGrid[(i + 1) % N, (j + 1) % N] 
-				index += oddGrid[(i - 1) % N, (j - 1) % N] 
+				index += oddGrid[(i - 1), (j + 1) % N] 
+				index += oddGrid[(i + 1), (j - 1) % N] 
+				index += oddGrid[(i + 1), (j + 1) % N] 
+				index += oddGrid[(i - 1), (j - 1) % N] 
  			
 			#state choices based on index and random number
 			if timestep % 2 == 0:
@@ -111,13 +114,9 @@ def update(timestep):
 			else:
 				evenGrid[i, j] = state
 
-	#receive top and bottom extension rows
-	req_up = comm.Irecv(top_halo, source=((rank-1)%size))
-	req_down = comm.Irecv(bot_halo, source=((rank+1)%size))
-
 	req_up.Wait()
 	req_down.Wait()
-	
+
 	#top update calculations
 	for j in range(N):
 		if timestep % 2 == 0:
@@ -126,6 +125,7 @@ def update(timestep):
 			state = oddGrid[0, j]
 	
 		index = 0
+
 		index += top_halo[(j-1) % N]
 		index += top_halo[j]
 		index += top_halo[(j+1) % N]
@@ -180,26 +180,27 @@ def update(timestep):
 		state = new_state(state, index)
 
 		if timestep % 2 == 0:
-			oddGrid[0, j] = state
+			oddGrid[height-1, j] = state
 		else:
-			evenGrid[0, j] = state
+			evenGrid[height-1, j] = state
 
 	comm.barrier()
 
-"""
-def printBoard(last_timestep):
+
+def printPart(last_timestep):
+	print("rank " + str(rank) + " printing..")
 	if last_timestep % 2 == 0:
-		for i in range(N):
+		for i in range(height):
 			for j in range(N):
 				sys.stdout.write(str(str(evenGrid[i, j]) + " "))
 			print("")
 	else:
-		for i in range(N):
+		for i in range(height):
 			for j in range(N):
 				sys.stdout.write(str(str(oddGrid[i, j]) + " "))
-			print("")
-"""
+			print("\n")
 
 for timestep in range(1, t+1):
 	update(timestep)
 
+printPart(t)
