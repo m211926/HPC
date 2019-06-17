3dd8bbdb6c7b55e6b3b0aeb2fe60d99fb9478967
Harrison Foley <m211926@roger.navydsrc.hpc.mil>
2019-06-17 10:55:08 -0500

    working version! (slight issue with wraparound)

--- sim/simCuda.cu
+++ sim/simCuda.cu
@@ -23,28 +23,29 @@ __global__ void setup_kernel(curandState * state){
 /**
  * Executes one timestep on a thread
  */
-__global__ void update(short * inGrid, short * outGrid, curandState * rand_state){
-	short y = (blockIdx.y * blockDim.y) + threadIdx.y;
-	short x = (blockIdx.x * blockDim.x) + threadIdx.x;
-	short N = blockDim.x * gridDim.x;
-	short id = x + (N * y);
-
+__global__ void update(int * inGrid, int * outGrid, curandState * rand_state){
+	int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+	int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int N = blockDim.x * gridDim.x;
+	int id = x + (N * y);
 	//generate random number
-	unsigned int randres = curand(&rand_state[id]);
-	randres = (short) (((int) randres) % 300);
-
-	short index = 0;
-	short state = inGrid[id];
+	unsigned int tmprandres = curand(&rand_state[id]);
+	int randres = ((int) tmprandres) % 500;
 	
+	int index = 0;
+	int state = inGrid[id];
+
 	//calculate index
 	index += inGrid[((x-1) % N) + N*y];
 	index += inGrid[((x+1) % N) + N*y];
 	index += inGrid[x + N*((y-1) % N)];
-	index += inGrid[x - N*((y+1) % N)];
+	index += inGrid[x + N*((y+1) % N)];
 	index += inGrid[((x+1) % N) + N*((y-1) % N)];
 	index += inGrid[((x-1) % N) + N*((y-1) % N)];
 	index += inGrid[((x+1) % N) + N*((y+1) % N)];
 	index += inGrid[((x-1) % N) + N*((y+1) % N)];
+	
+	//printf("x:%d y:%d id:%d State:%d Index:%d\n", x, y, id, state, index);
 	
 	//find new state
 	if(state == 0){
@@ -54,6 +55,7 @@ __global__ void update(short * inGrid, short * outGrid, curandState * rand_state
 		else state = 3;
 	}
 	else if(state == 1){
+		//printf("(%d,%d) s:%d i:%d\n", x, y, state, index);
 		if(randres == 0 || index > 16) state = 3;
 		else if(index < 1) state = 0;
 		else state = 1;
@@ -64,17 +66,20 @@ __global__ void update(short * inGrid, short * outGrid, curandState * rand_state
 		else state = 0;
 	}
 	else if(state == 3){
-		if(index > 9) state = 3;
+		if(index > 9){
+			state = 3;
+			//printf("I, (%d, %d), stuck because i=%d\n", x, y, index);
+		}
 		else state = 1;
 	}
-	
+	else printf("ERROR: (%d, %d) state out of bounds: %d\n", x, y, state);
 	//update relevant array
 	outGrid[id] = state;
 }
 
 
-void print_grid(short * grid, short* source_d, int N) {
-	cudaMemcpy(grid, source_d, N*N*sizeof(short), cudaMemcpyDeviceToHost);
+void print_grid(int * grid, int* source_d, int N) {
+	cudaMemcpy(grid, source_d, N*N*sizeof(int), cudaMemcpyDeviceToHost);
 	
 	for(int y = 0; y < N; y++){
 		for(int x = 0; x < N; x++){
@@ -114,27 +119,27 @@ int main(int argc, char * argv[]){
 	int blockw = N/common_divisor;
 
 	//blank grid (host)
-	short * blankGrid;
+	int * blankGrid;
 
 	//grid memory allocation (host)
-	blankGrid = (short*) calloc(N*N, sizeof(short));
+	blankGrid = (int*) calloc(N*N, sizeof(int));
 	printf("grid host allocation complete\n");
 
 	//grids (device)
-	short * evenGrid_d;
-	short * oddGrid_d;
+	int * evenGrid_d;
+	int * oddGrid_d;
 
 	//memory allocation (device)
-	cudaMalloc((void**) &evenGrid_d, N*N*sizeof(short));
-	cudaMalloc((void**) &oddGrid_d, N*N*sizeof(short));
+	cudaMalloc((void**) &evenGrid_d, N*N*sizeof(int));
+	cudaMalloc((void**) &oddGrid_d, N*N*sizeof(int));
 
 	printf("even/odd grid device allocation complete\n");
 	
 	//transfer CPU contents to GPU (all zeroed out)
 	for(int y = 0; y < N; y++){
 		for(int x = 0; x < N; x++){
-			cudaMemcpy(evenGrid_d, blankGrid, N*N*sizeof(short), cudaMemcpyHostToDevice);
-			cudaMemcpy(oddGrid_d, blankGrid, N*N*sizeof(short), cudaMemcpyHostToDevice);
+			cudaMemcpy(evenGrid_d, blankGrid, N*N*sizeof(int), cudaMemcpyHostToDevice);
+			cudaMemcpy(oddGrid_d, blankGrid, N*N*sizeof(int), cudaMemcpyHostToDevice);
 		}
 	}
 	
@@ -159,21 +164,31 @@ int main(int argc, char * argv[]){
 	setup_kernel<<<BLOCK_ARRANGEMENT,BLOCK_SHAPE>>>(states_d);
 	printf("device random initialization complete, begin sim\n");	
 
+	usleep(500000);
+	printf("In 3..\n");
+	sleep(1);
+	printf("2..\n");
+	sleep(1);
+	printf("1..\n");
+	sleep(1);
 	//call updates for each timestep
 	for(int i = 1; i < t+1; i++){
+		printf("t=%d\n",i);
 		if(i%2==0){
+			printf("in = even, out = odd\n");
 			update<<<BLOCK_ARRANGEMENT,BLOCK_SHAPE>>>(evenGrid_d, oddGrid_d, states_d);
 			print_grid(blankGrid, oddGrid_d, N);
 			for(int i = 0; i < N; i++) printf("*");
 			printf("\n");
 		}
 		else{
+			printf("in = odd, out = even\n");
 			update<<<BLOCK_ARRANGEMENT,BLOCK_SHAPE>>>(oddGrid_d, evenGrid_d, states_d);
 			print_grid(blankGrid, evenGrid_d, N);
 			for(int i = 0; i < N; i++) printf("*");
 			printf("\n");
 		}
-		//usleep(300000);
+		usleep(100000);
 	}
 
 	//*******************************************************************
